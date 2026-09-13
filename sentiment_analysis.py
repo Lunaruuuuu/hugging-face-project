62b650bec72b19b5271df3ba37c6c05cd38a1970
Ryu Nakata <ryunakata312@gmail.com>
2026-09-13 23:26:12 +0700

    Penambahan Input dan Screenshot hasil

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,13 +1,9 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch
 
-
 MODEL_NAME = "Aardiiiiy/indobertweet-base-Indonesian-sentiment-analysis"
 
-
-# Load tokenizer dan model
 print("Loading model...")
-
 tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME)
 
@@ -28,11 +24,7 @@ def predict_sentiment(text):
 
     probabilities = torch.softmax(outputs.logits, dim=1)
 
-    predicted_class = torch.argmax(
-        probabilities,
-        dim=1
-    ).item()
-
+    predicted_class = torch.argmax(probabilities, dim=1).item()
     confidence = probabilities[0][predicted_class].item()
 
     label = model.config.id2label[predicted_class]
@@ -40,22 +32,22 @@ def predict_sentiment(text):
     return label, confidence
 
 
-# Data uji buatan sendiri
-test_data = [
-    "Makanannya enak banget, porsinya juga banyak.",
-    "Makanannya gak enak, rasanya hambar banget.",
-    "Makanannya gak gak enak, masih lumayan kok."
-]
+print("\n=== Indonesian Sentiment Analysis ===")
+print("Ketik 'keluar' untuk mengakhiri program.\n")
 
+while True:
+    text = input("Masukkan kalimat: ")
 
-# Inference
-print("\n=== HASIL PREDIKSI ===\n")
+    if text.lower() == "keluar":
+        print("Program selesai.")
+        break
 
-for text in test_data:
+    if not text.strip():
+        print("Kalimat tidak boleh kosong.\n")
+        continue
 
     label, confidence = predict_sentiment(text)
 
-    print("Input      :", text)
     print("Sentiment  :", label)
     print("Confidence :", f"{confidence:.2%}")
-    print("-" * 60)
+    print("-" * 50)
